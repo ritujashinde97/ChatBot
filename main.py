0fab2655d585fa0c03cb117c9fa02ee342131ee5
Rituja Shinde <65076102+ritujashinde97@users.noreply.github.com>
2020-05-25 16:25:18 +0530

    text to speech

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from chatterbot.trainers import ListTrainer
-
 from chatterbot import ChatBot
 from tkinter import *
+import pyttsx3 as pp
+
+engine=pp.init()
+voices=engine.getProperty('voices')
+print(voices)
+engine.setProperty('voice',voices[0].id)
+
+def speak(word):
+    engine.say(word)
+    engine.runAndWait()
 
 bot = ChatBot("My Bot")
 
 convo = {
     'hello',
-    'hi there',
+    'hi',
     'what is your name?',
     'My name is Bot, I am created by Rituja ',
     'how are you?',
@@ -18,7 +27,11 @@ convo = {
     'Which language you prefer to talk?',
     'I mostly prefer English.',
     'I need your help.',
-    'how may I help you?'
+    'how may I help you?',
+    'what are your hobbies?',
+    'I love painting.',
+    'what do you like to eat?',
+    'I like punjabi food.',
 
 }
 
@@ -46,11 +59,13 @@ def ask_from_bot():
     answer_from_bot = bot.get_response(query)
     msgs.insert(END,"You : " + query)
     msgs.insert(END,"Bot : " + str(answer_from_bot))
+    speak(answer_from_bot)
     textField.delete(0,END)
+    msgs.yview(END)
 
 frame = Frame(main)
 sc=Scrollbar(frame)
-msgs=Listbox(frame,width=80,height=20)
+msgs=Listbox(frame,width=80,height=20, yscrollcommand=sc.set)
 sc.pack(side=RIGHT,fill=Y)
 msgs.pack(side=LEFT, fill=BOTH, pady=10)
 frame.pack()
@@ -58,4 +73,11 @@ textField=Entry(main,font=("Verdana",20))
 textField.pack(fill=X,pady=10)
 btn=Button(main,text="Ask The BOT",font=("Verdana",20),command=ask_from_bot)
 btn.pack()
+
+def enter_function(event):
+    btn.invoke()
+
+main.bind('<Return>',enter_function)
+
+
 main.mainloop()
